70d65d62dc6179fe478af5c59f3418998a380201
marcusplenty <44761537+marcusplenty@users.noreply.github.com>
2023-08-17 16:27:01 -0400

    accessibillity changes made

--- src/components/ImageDetail/imageDetail.jsx
+++ src/components/ImageDetail/imageDetail.jsx
@@ -5,38 +5,59 @@ import { useParams } from 'react-router-dom';
 const ImageDetail = () => {
   const { imageId } = useParams();
   const [imageDetails, setImageDetails] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
     const fetchImageDetails = async () => {
       try {
+        setLoading(true);
+        setError(null);
+
         const response = await fetch(
           `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&id=${imageId}`
         );
 
+        if (!response.ok) {
+          throw new Error('Failed to fetch image details');
+        }
+
         const data = await response.json();
+
         if (data.hits.length > 0) {
-          setImageDetails(data.hits[0]); // Assuming image data is an array
+          setImageDetails(data.hits[0]);
         } else {
-          setImageDetails(null); // Image not found
+          setError('Image details not found');
         }
       } catch (error) {
-        console.log(error);
+        setError('An error occurred while fetching image details');
+        console.error(error);
+      } finally {
+        setLoading(false);
       }
     };
 
     fetchImageDetails();
   }, [imageId]);
 
-  if (!imageDetails) {
+  if (loading) {
     return <div>Loading...</div>;
+  }
+
+  if (error) {
+    return <div>Error: {error}</div>;
   }
 
   return (
     <div>
-      <img src={imageDetails.webformatURL} alt={imageDetails.tags} />
+      <img
+        src={imageDetails.webformatURL}
+        alt={imageDetails.tags}
+        aria-describedby="image-description"
+      />
       <p>User: {imageDetails.user}</p>
       <p>Tags: {imageDetails.tags}</p>
-      <Link to="/">Back to Search</Link> 
+      <Link to="/" aria-label="Back to Search">Back to Search</Link>
     </div>
   );
 };
--- src/components/ImageSearch/imageSearch.jsx
+++ src/components/ImageSearch/imageSearch.jsx
@@ -1,40 +1,63 @@
 import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 
-
 const ImageSearch = () => {
   const [searchTerm, setSearchTerm] = useState('');
   const [images, setImages] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
 
   const handleSearch = async () => {
+    setLoading(true);
+    setError(null);
 
     try {
       const response = await fetch(
         `https://pixabay.com/api/?key=${process.env.REACT_APP_PIXABAY_API_KEY}&q=${searchTerm}&image_type=photo`
       );
 
+      if (!response.ok) {
+        throw new Error('Failed to fetch images');
+      }
+
       const data = await response.json();
       setImages(data.hits);
     } catch (error) {
-      console.log(error);
+      setError('An error occurred while fetching images');
+      console.error(error);
+    } finally {
+      setLoading(false);
     }
   };
-  
 
   return (
     <div>
+      <label htmlFor="searchInput">Search for images:</label>
       <input
         type="text"
-        placeholder="Search for images"
+        id="searchInput"
+        placeholder="e.g. mountains, beach, animals..."
         value={searchTerm}
         onChange={(e) => setSearchTerm(e.target.value)}
+        aria-label="Search input"
       />
-      <button onClick={handleSearch}>Search</button>
+      <button
+        onClick={handleSearch}
+        disabled={loading || !searchTerm}
+        aria-label="Search button"
+      >
+        {loading ? 'Searching...' : 'Search'}
+      </button>
 
-      <div>
-      {images.map((image) => (
-          <Link key={image.id} to={`/image/${image.id}`}>
-            <img src={image.previewURL} alt={image.tags} />
+      <div className="image-container">
+        {error && <div className="error-message">{error}</div>}
+        {images.map((image) => (
+          <Link key={image.id} to={`/image/${image.id}`} tabIndex="0">
+            <img
+              src={image.previewURL}
+              alt={image.tags}
+              className="image-item"
+            />
           </Link>
         ))}
       </div>
